7ba9b49812f4e7fdb40e1f38adecf71c6f0fc946
Kate Lormor <kate.lormor@googlemail.com>
2024-03-06 16:46:01 +1100

    Date is now live

--- src/index.js
+++ src/index.js
@@ -2,8 +2,65 @@ function refreshWeather(response) {
   let tempElement = document.querySelector("#temp");
   let temp = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  let feelsLikeElement = document.querySelector("#feels-like");
+  let feelsLike = response.data.temperature.feels_like;
+  let humidityElemenet = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  console.log(new Date());
+
   tempElement.innerHTML = Math.round(temp);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  feelsLikeElement.innerHTML = `${Math.round(feelsLike)}ºC`;
+  humidityElemenet.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}kmph`;
+  timeElement.innerHTML = formatDate(date);
+
+
+}
+
+function formatDate(date) {
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  let dates = date.getDate();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+  let year = date.getFullYear();
+
+  return `${hour}:${minute} ${day} ${dates} ${month} ${year}`;
+  //return "11:24 Wednesday 6th March 2024";
 }
 
 function searchCity(city) {
