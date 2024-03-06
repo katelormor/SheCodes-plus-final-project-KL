7a7cee81f851484913119f6aea72ac82c2ecdb0c
Kate Lormor <kate.lormor@googlemail.com>
2024-03-06 15:11:43 +1100

    JS search function using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
+function refreshWeather(response) {
+  let tempElement = document.querySelector("#temp");
+  let temp = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  tempElement.innerHTML = Math.round(temp);
+}
+
+function searchCity(city) {
+  let apiKey = `82atb6o4834371cad1102b496a40f6fb`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function submitSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", submitSearch);
+
+searchCity("London");
